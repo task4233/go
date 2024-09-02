75c3b26cf2bc0f77af70b503fc57860df2a9b308
task4233 <29667656+task4233@users.noreply.github.com>
2024-09-02 22:05:56 +0900

    change(escape): add statement for escape analysis

--- src/cmd/compile/internal/escape/stmt.go
+++ src/cmd/compile/internal/escape/stmt.go
@@ -74,6 +74,11 @@ func (e *escape) stmt(n ir.Node) {
 		e.block(n.Body)
 		e.block(n.Else)
 
+	case ir.OUNLESS:
+		n := n.(*ir.UnlessStmt)
+		e.discard(n.Cond)
+		e.block(n.Body)
+
 	case ir.OCHECKNIL:
 		n := n.(*ir.UnaryExpr)
 		e.discard(n.X)
@@ -81,6 +86,14 @@ func (e *escape) stmt(n ir.Node) {
 	case ir.OFOR:
 		n := n.(*ir.ForStmt)
 		base.Assert(!n.DistinctVars) // Should all be rewritten before escape analysis
+		e.loopDepth++
+		e.discard(n.Cond)
+		e.stmt(n.Post)
+		e.block(n.Body)
+		e.loopDepth--
+
+	case ir.OFOUR:
+		n := n.(*ir.FourStmt)
 		e.loopDepth++
 		e.discard(n.Cond)
 		e.stmt(n.Post)
