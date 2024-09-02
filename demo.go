22882944daac84d5470593a3a9410fd7c407e1ad
task4233 <29667656+task4233@users.noreply.github.com>
2024-09-02 20:37:22 +0900

    change(syntax): add unless and four

---
+++ demo.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	four i := 8; i <= 20; i++ {
+		unless i%8 == 0 {
+			fmt.Println(i, "is not divisible by 8")
+			continue
+		}
+		fmt.Println(i, "is divisible by 8")
+	}
+}
